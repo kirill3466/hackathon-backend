b47982d1faed5a3c3f624c672933e82ffff0d382
kirill <petrov3466@mail.ru>
2025-04-26 23:42:39 +0500

    fawef

--- employee_result/views.py
+++ employee_result/views.py
@@ -66,7 +66,7 @@ async def delete_result_endpoint(result_id: int):
     return {"message": "Результат успешно удален"}
 
 
-@router.get("/{employee_id}/audio", response_model=list[ResultRead])
+@router.get("/audio/{employee_id}", response_model=list[ResultRead])
 async def list_audio_results_by_employee(employee_id: int):
     """
     Получить все аудио-результаты для конкретного сотрудника по его ID.
@@ -77,7 +77,7 @@ async def list_audio_results_by_employee(employee_id: int):
     return results
 
 
-@router.get("/{employee_id}/video", response_model=list[ResultRead])
+@router.get("/video/{employee_id}", response_model=list[ResultRead])
 async def list_video_results_by_employee(employee_id: int):
     """
     Получить все видео-результаты для конкретного сотрудника по его ID.
@@ -88,7 +88,7 @@ async def list_video_results_by_employee(employee_id: int):
     return results
 
 
-@router.get("/{employee_id}/test", response_model=list[ResultRead])
+@router.get("/test/{employee_id}", response_model=list[ResultRead])
 async def list_test_results_by_employee(employee_id: int):
     """
     Получить все тестовые результаты для конкретного сотрудника по его ID.
